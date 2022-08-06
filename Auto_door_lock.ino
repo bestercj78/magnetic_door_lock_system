f2d2bfd86fba689828137412e6b861b731a4813d
bestercj78 <110714698+bestercj78@users.noreply.github.com>
2022-08-06 09:51:17 +0200

    Update Auto_door_lock.ino

--- Auto_door_lock.ino
+++ Auto_door_lock.ino
@@ -119,51 +119,6 @@ void loop() {
             inputUser = 1;
             openDoor(inputUser);
             break;
-
-          case 2:
-            inputUser = 2;
-            openDoor(inputUser);
-            break;
-
-          case 3:
-            inputUser = 3;
-            openDoor(inputUser);
-            break;
-
-          case 4:
-            inputUser = 4;
-            openDoor(inputUser);
-            break;
-
-          case 5:
-            inputUser = 5;
-            openDoor(inputUser);
-            break;
-
-          case 6:
-            inputUser = 6;
-            openDoor(inputUser);
-            break;
-
-          case 7:
-            inputUser = 7;
-            openDoor(inputUser);
-            break;
-
-          case 8:
-            inputUser = 8;
-            openDoor(inputUser);
-            break;
-
-          case 9:
-            inputUser = 9;
-            openDoor(inputUser);
-            break;
-
-          case 10:
-            inputUser = 10;
-            openDoor(inputUser);
-            break;
         }
       }
     }
@@ -198,42 +153,6 @@ int openDoor(int inputUser) {
 
     case 1:
       user = "CJ";
-      break;
-
-    case 2:
-      user = "CJ";
-      break;
-
-    case 3:
-      user = "Chris";
-      break;
-
-    case 4:
-      user = "Chris";
-      break;
-
-    case 5:
-      user = "Niecy";
-      break;
-
-    case 6:
-      user = "Niecy";
-      break;
-
-    case 7:
-      user = "Kyle";
-      break;
-
-    case 8:
-      user = "Kyle";
-      break;
-
-    case 9:
-      user = "Ciara";
-      break;
-
-    case 10:
-      user = "Ciara";
       break;
   }
 
